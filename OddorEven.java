1ca41f654e3868cbcd2c35e04344799ae7abdf04
SoundharyaSA <soundharya.amarnath@gmail.com>
2024-02-28 12:14:43 +0530

    added oddoreven.java

---
+++ OddorEven.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class OddorEven{
+public static void main(){
+Scanner sc=new Scanner();
+int a=sc.nextInt();
+if(a%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}
+}
